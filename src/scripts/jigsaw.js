d1c503aba67585ef8005e7e0f4d7689e718a977b
ewfian <ewfian@vip.qq.com>
2018-01-19 01:07:45 +0800

    update [Jigsaw]

--- src/scripts/jigsaw.js
+++ src/scripts/jigsaw.js
@@ -1,14 +1,13 @@
 var Jigsaw = (function () {
 
-    var Jigsaw = function (query, sizeX, sizeY) {
+    var Jigsaw = function (query) {
         this.container = document.querySelector(query);
-        this.sizeX = sizeX || 3;
-        this.sizeY = sizeY || 3;
-
     };
 
-    var imageWidth = 300,
-        imageHeight = 300;
+    var reverseMoving = true;
+
+    var imageWidth = 600,
+        imageHeight = 600;
 
     var JigsawSizeX = 0,
         JigsawSizeY = 0,
@@ -24,20 +23,13 @@ var Jigsaw = (function () {
 
     Jigsaw.prototype.play = function () {
         var root = this.container;
-        var play = root.querySelector('.play-panel');
 
-        var _jigsaw = initialize(this.sizeX, this.sizeY);
-        play.appendChild(_jigsaw);
+        initialize(root);
 
-        animate();
-
-        bindEvent(_jigsaw);
+        bindEvent(root);
     };
 
-    var initialize = function (sizeX, sizeY) {
-
-        JigsawSizeX = sizeX;
-        JigsawSizeY = sizeY;
+    var createBlocks = function () {
 
         JigsawWidth = imageWidth;
         JigsawHeight = imageHeight;
@@ -64,6 +56,7 @@ var Jigsaw = (function () {
             length: blocksLength
         }).map(Number.call, Number).sort(() => 0.5 - Math.random());
 
+        // randomOrder = [2, 3, 4, 5, 6, 7, 8, 1, 0];
 
         for (var i = 0; i < blocksLength; i++) {
             var item = document.createElement('div');
@@ -80,30 +73,31 @@ var Jigsaw = (function () {
                 offsetY = -(Math.floor(leftIndex / JigsawSizeX) * blockHeight),
                 backgroundImage = 'url(' + imageURL + ') ' + offsetX + 'px ' + offsetY + 'px ' + 'no-repeat';
 
-            var s = {
+            var blockStyle = {
                 width: Math.ceil(blockWidth) + 'px',
                 height: Math.ceil(blockHeight) + 'px',
                 background: backgroundImage,
-                left: locTopCurrent + 'px',
-                top: locLeftCurrent + 'px',
+                top: locTopActually + 'px',
+                left: locLeftActually + 'px',
                 zIndex: leftIndex
             };
+
             item.setAttribute('orderIndex', orderIndex);
 
-            for (var j in s) {
-                item.style[j] = s[j];
+            for (var j in blockStyle) {
+                item.style[j] = blockStyle[j];
             }
 
             list.appendChild(item);
             blocksArray.push({
                 'element': item,
                 'locCurrent': {
-                    'locLeft': locLeftCurrent,
-                    'locTop': locTopCurrent
+                    'left': locLeftCurrent,
+                    'top': locTopCurrent
                 },
                 'locActually': {
-                    'locLeft': locLeftActually,
-                    'locTop': locTopActually
+                    'left': locLeftActually,
+                    'top': locTopActually
                 }
             });
 
@@ -112,63 +106,146 @@ var Jigsaw = (function () {
         return list;
     };
 
-    function quad(timeFraction) {
-        if (timeFraction == 0) timeFraction = 2;
-        return Math.pow(timeFraction, 2);
-    }
 
     var animate = function () {
-        var item = blocksArray[0];
-        var left = item.locActually.locLeft;
-        var top = item.locActually.locTop;
-        var id = setInterval(frame, 10);
 
-        function frame() {
-            if (left == item.locCurrent.locLeft || top == item.locCurrent.locTop) {
-                clearInterval(id);
-                console.log('object');
-            } else {
-                left = quad(left);
-                top = quad(top);
-                item.element.style.top = top + 'px';
-                item.element.style.left = left + 'px';
+        var MovingThing = (function () {
+            var MovingThing = function (from, to) {
+                if (reverseMoving) {
+                    this.from = from;
+                    this.to = to;
+                } else {
+                    this.from = to;
+                    this.to = from;
+                }
+            };
+            MovingThing.prototype.threshold = 0;
+
+            MovingThing.prototype.crease = function (increase, decrease) {
+                if (this.to - this.from > this.threshold) {
+                    this.from = increase(this.from);
+                } else if (this.to - this.from < this.threshold) {
+                    this.from = decrease(this.from);
+                }
+            };
+            MovingThing.prototype.isFinished = function () {
+                return Math.abs(this.to - this.from) <= this.threshold;
+            };
+
+            MovingThing.prototype.current = function () {
+                return this.from;
+            };
+
+            return MovingThing;
+        })();
+
+        var creaseFunction = {
+            'increase': i => i + 2,
+            'decrease': i => i - 2
+        };
+
+        var moveOneBlock = function (item) {
+            var movingX = new MovingThing(item.locActually.left, item.locCurrent.left);
+            var movingY = new MovingThing(item.locActually.top, item.locCurrent.top);
+            var interval = setInterval(frame, 1);
+
+            function frame() {
+                if (movingX.isFinished() && movingY.isFinished()) {
+                    clearInterval(interval);
+                } else {
+                    movingX.crease(creaseFunction.increase, creaseFunction.decrease);
+                    movingY.crease(creaseFunction.increase, creaseFunction.decrease);
+                    item.element.style.left = movingX.from + 'px';
+                    item.element.style.top = movingY.from + 'px';
+                }
             }
-            console.log([item.locActually.locLeft, item.locCurrent.locLeft, left]);
-            console.log([item.locActually.locTop, item.locCurrent.locTop, top]);
-        }
+        };
+
+        // console.log(blocksArray);
+
+        blocksArray.forEach(block => {
+            moveOneBlock(block);
+        });
     };
 
-    var bindEvent = function (_jigsaw) {
-        _jigsaw.addEventListener('mouseover', mouseOverHandler);
-        _jigsaw.addEventListener('mouseout', mouseOutHandler);
+    var initialize = function (root) {
+        var messing = root.querySelector('#messing'),
+            solving = root.querySelector('#solving');
+
+        var play = root.querySelector('.play-panel');
+
+
+        messing.addEventListener('click', () => {
+
+            play.innerHTML = '';
+
+            var row = root.querySelector('#row'),
+                col = root.querySelector('#column');
+
+            JigsawSizeX = row.value || 3;
+            JigsawSizeY = col.value || 3;
+
+
+            var _jigsaw = createBlocks();
+
+            play.appendChild(_jigsaw);
+            reverseMoving = true;
+            animate();
+        });
+
+        solving.addEventListener('click', () => {
+            reverseMoving = false;
+            animate();
+        });
+    };
+
+    var bindEvent = function (root) {
+        var _jigsaw = root.querySelector('.play-panel');
+        //     _jigsaw.addEventListener('mouseover', mouseOverHandler);
+        //     _jigsaw.addEventListener('mouseout', mouseOutHandler);
         _jigsaw.addEventListener('mousedown', mouseDownHandler);
-        _jigsaw.addEventListener('mouseup', mouseUpHandler);
-
-        function mouseOverHandler(event) {
-            var target = event.target;
-            console.log('mouseOverHandler', target);
-        }
+        document.addEventListener('mouseup', mouseUpHandler);
 
 
-        function mouseOutHandler(event) {
-            var target = event.target;
-            console.log(target);
-
-        }
+        var selectedItem = null;
+        var offsetX = null,
+            offsetY = null;
 
         function mouseDownHandler(event) {
             var target = event.target;
-            console.log(target);
-
+            selectedItem = target;
+            document.addEventListener('mousemove', mouseMoveHandler);
+            offsetX = event.offsetX;
+            offsetY = event.offsetY;
         }
 
 
         function mouseUpHandler(event) {
-            var target = event.target;
-            console.log(target);
+            if (selectedItem) {
+                document.removeEventListener('mousemove', mouseMoveHandler);
+            }
+        }
+
+        function mouseMoveHandler(event) {
+
+            var mouseX = event.clientX,
+                mouseY = event.clientY;
+
+            event.preventDefault();
+
+            var panel = root.querySelector('.play-panel');
+
+            selectedItem.style.left = (mouseX - panel.getBoundingClientRect().left - offsetX - 20) + 'px';
+            selectedItem.style.top = (mouseY - panel.getBoundingClientRect().top - offsetY - 20) + 'px';
+
+            // blocksArray.forEach(block => {
+            //     if(block.element == selectedItem){
+            //         block.locActually.left = selectedItem.style.left;
+            //         block.locActually.top = selectedItem.style.top;
+            //     }
+            // });
         }
     };
-
 
     return Jigsaw;
 })();
